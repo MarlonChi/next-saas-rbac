89602e81625ec1d9033cc5bbe297bd9b8042666d
MarlonChi <marlonchiodelli@hotmail.com>
2024-07-19 21:59:06 -0300

    feat: autoJoinOrganization

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -26,13 +26,27 @@ export async function createAccount(app: FastifyInstance) {
             return reply.status(400).send({ message: 'User with same e-mail already exists.' })
         }
 
+        const [, domain] = email.split('@')
+
+        const autoJoinOrganization = await prisma.organization.findFirst({
+            where: {
+                domain,
+                shouldAttachUsersByDomain: true
+            }
+        })
+
         const passwordHash = await hash(password, 6)
 
         await prisma.user.create({
             data: {
                 name,
                 email,
-                passwordHash
+                passwordHash,
+                member_on: autoJoinOrganization ? {
+                    create: {
+                        organizationId: autoJoinOrganization.id,
+                    }
+                } : undefined
             }
         })
 
