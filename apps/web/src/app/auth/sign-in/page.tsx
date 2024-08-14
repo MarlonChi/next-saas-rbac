ad2d44108a0235a3210b17e613f613da30cfab2c
MarlonChi <marlonchiodelli@hotmail.com>
2024-08-13 22:34:45 -0300

    feat: sign in server actions in progress

--- apps/web/src/app/auth/sign-in/page.tsx
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -6,11 +6,13 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
+import { signInWithEmailAndPassword } from './actions'
+
 import githubIcon from '@/assets/github.svg'
 
 export default function SignInPage() {
   return (
-    <form action="" className="space-y-4">
+    <form action={signInWithEmailAndPassword} className="space-y-4">
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
