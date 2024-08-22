789e93a3d15500cccd59f251a8d364676a989227
MarlonChi <marlonchiodelli@hotmail.com>
2024-08-21 21:26:05 -0300

    feat: intercept routes to switch

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -2,17 +2,19 @@ import { Header } from '@/components/header'
 
 export default async function AuthLayout({
   children,
+  sheet,
 }: Readonly<{
   children: React.ReactNode
+  sheet: React.ReactNode
 }>) {
   //   if (isAuthenticated()) {
   //     redirect('/auth/sign-in')
   //   }
 
   return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
-    </div>
+    <>
+      {children}
+      {sheet}
+    </>
   )
 }
