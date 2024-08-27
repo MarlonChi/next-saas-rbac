9f4d73bd323261a907a412672b7ef2a3f7cdd399
MarlonChi <marlonchiodelli@hotmail.com>
2024-08-27 20:51:39 -0300

    feat: config org page

--- apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -1,6 +1,6 @@
 import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
-import { OrganizationForm } from '../../create-organization/organization-form'
 import { InterceptedSheetContent } from '@/components/intercepted-sheet-content'
+import { OrganizationForm } from '../../org/organization-form'
 
 export default function CreateOrganization() {
   return (
--- apps/web/src/app/(app)/org/organization-form.tsx
+++ apps/web/src/app/(app)/org/organization-form.tsx
@@ -8,7 +8,7 @@ import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useFormState } from '@/hooks/use-form-state'
-import { createOrganizationAction } from './actions'
+import { createOrganizationAction } from '../create-organization/actions'
 
 export function OrganizationForm() {
   const [{ errors, message, success }, handleSubmit, isPending] = useFormState(
